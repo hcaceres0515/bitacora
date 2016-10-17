156633e49da1d7a5673c4ec75d9bbbee3a2980d7
jerherrerar <jerherrerar@gmail.com>
2016-10-16 23:07:12 -0500

    Create users, routes, locations

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var ctrlUsers = require('../controllers/users');
+var ctrlRoutes = require('../controllers/routes');
+var ctrlLocations = require('../controllers/locations');
+
 
 router.get('/getUsers', ctrlUsers.getAllUsers);
 router.post('/createUser', ctrlUsers.createUser);
+router.put('/updateUser/:userid', ctrlUsers.updateUser);
+router.get('/getUser/:userid', ctrlUsers.getUser);
+router.delete('/deleteUser/:userid', ctrlUsers.deleteUser);
+
+router.post('/users/:userid/routes', ctrlRoutes.routesCreate);
+
+router.post('/users/:userid/routes/:routeid/locations', ctrlLocations.locationsCreate);
+
 
 /*router.get('/getlocations', ctrlLocations.getAllLocations);
 router.get('/locations', ctrlLocations.locationsListByDistance);
