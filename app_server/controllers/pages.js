5d192eea568e551006ae6a414b0c450008b6fbed
harold <harold.caceres.zea@gmail.com>
2016-11-21 15:38:13 -0500

    fixed front and models 2

--- app_server/controllers/pages.js
+++ app_server/controllers/pages.js
@@ -3,7 +3,7 @@ var apiOptions = {
     server : "http://localhost:3000"
 };
 if (process.env.NODE_ENV === 'production') {
-    apiOptions.server = "https://bitacorasocial.herokuapp.com/";
+    apiOptions.server = "https://bitacorasocial.herokuapp.com";
 }
 
 
