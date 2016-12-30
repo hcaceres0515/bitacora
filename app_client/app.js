95da9d29add3596b399dc6cbe70ed5734846b1f1
harold <harold.caceres.zea@gmail.com>
2016-12-30 11:39:01 -0500

    update views

--- app_client/app.js
+++ app_client/app.js
@@ -31,10 +31,28 @@ function config($routeProvider, $locationProvider){
 angular
 	.module('bitacora')
 	.config(['$routeProvider', '$locationProvider', config]);
-
+/*
 $.getScript( "https://maps.googleapis.com/maps/api/js?key=AIzaSyCHOdauoh_16stWPxM67AJZpWjp3UWA2rI&libraries=places&callback=initialize", function( data, textStatus, jqxhr ) {
   //console.log( data ); // Data returned
   console.log( textStatus ); // Success
   console.log( jqxhr.status ); // 200
   console.log( "Google Maps - Load was performed." );
-});
+});*/
+
+    function loadScript(src,callback){
+  
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    if(callback)script.onload=callback;
+    document.getElementsByTagName("head")[0].appendChild(script);
+    script.src = src;
+  }
+  
+  
+  loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCHOdauoh_16stWPxM67AJZpWjp3UWA2rI&libraries=places&callback=initialize',
+              function(){log('google-loader has been loaded, but not the maps-API ');});
+
+
+              function log(str){
+  document.getElementsByTagName('pre')[0].appendChild(document.createTextNode('['+new Date().getTime()+']\n'+str+'\n\n'));
+}
